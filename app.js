f25a91a1bef8c187945e50a7a08684021696e254
Danny Li <Danny.Li@clemenger.com.au>
2018-01-10 12:21:25 +1100

    cheer sound, sound graph

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 
 var sound;
+var fft;
 
 function preload() {
-    sound = loadSound('boo1.mp3');
+    sound = loadSound('https://raw.githubusercontent.com/dannyli0109/sound_test/master/boo1.mp3');
 }
 
 function setup() {
+    createCanvas(512, 512);
+
     sound.play();
+    fft = new p5.FFT(0, 512)
+    frameRate(60)
+
 }
 
 function draw() {
+    // console.log(fft.analyze())
+    var spectrum = fft.analyze()
+    background(0);
+    stroke(255)
+    for (var i = 0; i < spectrum.length; i++) {
+        var height = map(spectrum[i], 0, 256, 0, 512)
+        rect(i, 512 - height, 1, height)
+    }
 
 }
