b7f564d080bab848e9534942af598f5d08005145
Danny Li <Danny.Li@clemenger.com.au>
2018-01-10 15:42:00 +1100

    added sounds

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var booSound;
 var fft;
 
 function preload() {
-    booSound = loadSound('https://raw.githubusercontent.com/dannyli0109/sound_test/master/cheer1.mp3');
+    booSound = loadSound('https://raw.githubusercontent.com/dannyli0109/sound_test/master/sounds/cheer.wav');
 }
 
 function setup() {
     createCanvas(512, 512);
 
-    fft = new p5.FFT(0.9, 128)
+    fft = new p5.FFT(0.8, 128)
     frameRate(60)
 
     booSound.onended(function () {
@@ -31,7 +31,7 @@ function draw() {
     stroke(255)
     for (var i = 0; i < spectrum.length; i++) {
         var height = map(spectrum[i], 0, 256, 0, 512)
-        rect(i * 4, 512 - height, 1, height)
+        rect(i * 8, 512 - height, 1, height)
     }
 }
 
